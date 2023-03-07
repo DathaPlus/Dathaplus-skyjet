ea70d78255a20c80e63afc14acd3ea39d3fad4b4
danidaniel6462 <daniel@dathaplus.com>
2023-03-07 07:51:03 -0500

    [DT+] add workflow to FTP deploy

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -1,7 +1,7 @@
-const React = require("react");
-const { MainLayout } = require("./src/components/layout/index");
-require("./node_modules/@dathaplus/storybook/dist/styles/style.min.css");
-require("./node_modules/@dathaplus/storybook/dist/styles/fonts.min.css");
+const React = require('react');
+const { MainLayout } = require('./src/components/layout/index');
+require('./node_modules/@dathaplus/storybook/dist/styles/style.min.css');
+require('./node_modules/@dathaplus/storybook/dist/styles/fonts.min.css');
 
 exports.wrapPageElement = ({ element, props }) => {
   return <MainLayout {...props}>{element}</MainLayout>;
